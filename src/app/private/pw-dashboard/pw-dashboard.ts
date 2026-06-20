d28d9f57453593ccc9f894c61acabf07a45238f6
project-githab <project.githab@gmail.com>
2026-06-20 18:48:55 +1100

    Commit

--- src/app/private/pw-dashboard/pw-dashboard.ts
+++ src/app/private/pw-dashboard/pw-dashboard.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-dashboard',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage],
   templateUrl: './pw-dashboard.html',
   styleUrl: './pw-dashboard.css',
 })
