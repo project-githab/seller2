2d7bf659eb31100316eb69627e917625282c18f0
project-githab <project.githab@gmail.com>
2026-06-21 18:53:00 +1100

    Commit

--- src/app/private/pw-inventory/pw-inventory.ts
+++ src/app/private/pw-inventory/pw-inventory.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-inventory',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage],
   templateUrl: './pw-inventory.html',
   styleUrl: './pw-inventory.css',
 })
