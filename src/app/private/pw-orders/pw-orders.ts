c5194bcc63b2bd0934b0592fcf83fcf86d46d71e
project-githab <project.githab@gmail.com>
2026-06-23 07:39:03 +1100

    Commit

--- src/app/private/pw-orders/pw-orders.ts
+++ src/app/private/pw-orders/pw-orders.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-orders',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage],
   templateUrl: './pw-orders.html',
   styleUrl: './pw-orders.css',
 })
