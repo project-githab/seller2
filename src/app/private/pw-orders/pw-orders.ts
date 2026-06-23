2aa265ab6af4fcac5658e41f82970859d358721b
project-githab <project.githab@gmail.com>
2026-06-24 05:19:29 +1100

    Commit

--- src/app/private/pw-orders/pw-orders.ts
+++ src/app/private/pw-orders/pw-orders.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
 import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pw-orders',
-  imports: [CpHeader, CpMenuList, NgOptimizedImage],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage, FormsModule],
   templateUrl: './pw-orders.html',
   styleUrl: './pw-orders.css',
 })
-export class PwOrders {}
+export class PwOrders {
+  selectedDate = '';
+}
