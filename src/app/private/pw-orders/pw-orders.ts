6a40eae70d98e9874953822f2f7e94acf3fd352f
project-githab <project.githab@gmail.com>
2026-06-25 04:06:22 +1100

    Commit

--- src/app/private/pw-orders/pw-orders.ts
+++ src/app/private/pw-orders/pw-orders.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
 import { NgOptimizedImage } from '@angular/common';
@@ -12,4 +12,14 @@ import { FormsModule } from '@angular/forms';
 })
 export class PwOrders {
   selectedDate = '';
+
+  protected readonly filterOpen = signal(false);
+
+  openFilter(): void {
+    this.filterOpen.set(true);
+  }
+
+  closeFilter(): void {
+    this.filterOpen.set(false);
+  }
 }
