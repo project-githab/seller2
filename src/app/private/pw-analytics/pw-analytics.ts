0b6efd628cb296073467aab68b9af65e9805aecc
project-githab <project.githab@gmail.com>
2026-06-26 22:49:35 +1100

    Commit

--- src/app/private/pw-analytics/pw-analytics.ts
+++ src/app/private/pw-analytics/pw-analytics.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-analytics',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, ReactiveFormsModule, FormsModule, NgOptimizedImage],
   templateUrl: './pw-analytics.html',
   styleUrl: './pw-analytics.css',
 })
