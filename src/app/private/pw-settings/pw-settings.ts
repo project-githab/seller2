20f20e8e749e154ae320eae77f69c80d014725b1
project-githab <project.githab@gmail.com>
2026-06-27 11:31:46 +1100

    Commit

--- src/app/private/pw-settings/pw-settings.ts
+++ src/app/private/pw-settings/pw-settings.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-settings',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage],
   templateUrl: './pw-settings.html',
   styleUrl: './pw-settings.css',
 })
