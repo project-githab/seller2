147b1da01db266ae7d7d3f73f5f5ad1d03cf0f9a
project-githab <project.githab@gmail.com>
2026-06-27 18:44:10 +1100

    Commit

--- src/app/private/pw-settings/pw-settings.ts
+++ src/app/private/pw-settings/pw-settings.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
 import { NgOptimizedImage } from '@angular/common';
@@ -9,4 +9,19 @@ import { NgOptimizedImage } from '@angular/common';
   templateUrl: './pw-settings.html',
   styleUrl: './pw-settings.css',
 })
-export class PwSettings {}
+export class PwSettings {
+  colorCodeMain = signal('#FFDC26');
+  colorCodeMainComplementary = signal('#2dd500');
+
+  onColorChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    // Записываем новое HEX-значение в сигнал
+    this.colorCodeMain.set(inputElement.value);
+  }
+
+  onColorChangeComplementary(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    // Записываем новое HEX-значение в сигнал
+    this.colorCodeMainComplementary.set(inputElement.value);
+  }
+}
