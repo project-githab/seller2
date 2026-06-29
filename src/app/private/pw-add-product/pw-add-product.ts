8a5f9a9a10b360c1dce007c061193e0741cc8f72
project-githab <project.githab@gmail.com>
2026-06-29 19:53:12 +1100

    Commit

--- src/app/private/pw-add-product/pw-add-product.ts
+++ src/app/private/pw-add-product/pw-add-product.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CpHeader } from '../../shared/private/cp-header/cp-header';
 import { CpMenuList } from '../../shared/private/cp-menu-list/cp-menu-list';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-pw-add-product',
-  imports: [CpHeader, CpMenuList],
+  imports: [CpHeader, CpMenuList, NgOptimizedImage],
   templateUrl: './pw-add-product.html',
   styleUrl: './pw-add-product.css',
 })
